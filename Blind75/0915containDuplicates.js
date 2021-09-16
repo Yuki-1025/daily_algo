0c76748ce08830c06d808ca80cdf2ef29eb1025d
Yuki Li <yunqili1025@gmail.com>
2021-09-16 11:31:44 -0400

    add solution 2&3

--- Blind75/0915containDuplicates.js
+++ Blind75/0915containDuplicates.js
@@ -5,7 +5,7 @@
 // a boolean: true or false
 // constraints:
 // edge case: [] => false; [1] => false;
-// time complexity: O(n)
+// time complexity: O(n) - all 3 solutions
 
 var containsDuplicate = function(nums) {
   // edge cases
@@ -20,6 +20,28 @@ var containsDuplicate = function(nums) {
       } else {
           return true;
       }
+  }
+  return false;
+};
+
+// SOLUTION 2 using set
+var containsDuplicate = function(nums) {
+  var toSet = new Set();
+  for (let i = 0; i < nums.length; i ++) {
+      if (toSet.has(nums[i])) {
+          return true;
+      } else {
+          toSet.add(nums[i]);
+      }
+  }
+  return false;
+};
+
+// SOLUTION 3
+var containsDuplicate = function(nums) {
+  var toSet = new Set(nums);
+  if (toSet.size !== nums.length) {
+      return true;
   }
   return false;
 };
