295049534d9eca7427e847f6ddaa53027e6be822
Yuki Li <yunqili1025@gmail.com>
2021-09-22 15:00:42 -0400

    solve sum of 2

---
+++ Blind75/0922sumOf2integers.js
@@ -0,0 +1,35 @@
+//Given two integers a and b, return the sum of the two integers without using the
+//operators + and -.
+
+// input: two integers
+// output: sum, integer
+// constraints: no + -
+// edge case: na
+
+var getSum = function(a, b) {
+  // check if negative
+  if (a < 0 && b < 0) {
+      let arr1 = Array(-a);
+      let arr2 = Array(-b);
+      let sum = arr1.concat(arr2);
+      return -sum.length;
+  }
+  if (a >= 0 && b >= 0) {
+      let arr1 = Array(a);
+      let arr2 = Array(b);
+      let sum = arr1.concat(arr2);
+      return sum.length;
+  }
+  if (a < 0 && -a <= b) {
+      return Array(b).slice(-a).length;
+  }
+  if (a < 0 && -a > b) {
+      return -Array(-a).slice(b).length;
+  }
+  if (b < 0 && -b <= a) {
+      return Array(a).slice(-b).length;
+  }
+  if (b < 0 && -b > a) {
+      return -Array(-b).slice(a).length;
+  }
+};
