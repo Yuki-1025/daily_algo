4275b4ad33fbb3dde4052d855a961e9e4a3733fb
Yuki Li <yunqili1025@gmail.com>
2021-09-22 17:47:26 -0400

    complete find first and last position in sorted array

--- Blind75/0922findFirstLastPositionSortedArr.js
+++ Blind75/0922findFirstLastPositionSortedArr.js
@@ -11,6 +11,54 @@
 // edgecases: cannot find target => [-1,-1]; only have one matching value => [index, index]
 // [] => [-1, -1]
 
+//complexity O(log n)
+var searchRange = function(nums, target) {
+  const findFirst = (nums, target) => {
+      let start = 0, end = nums.length - 1;
+      let mid = Math.floor((start + end) / 2);
+      while (start <= end) {
+          let mid = Math.floor((start + end) / 2);
+          if (nums[mid] === target) {
+              if (mid === 0 || nums[mid - 1] !== target) {
+                  return mid;
+              } else {
+                  //search left
+                  end = mid - 1;
+              }
+          } else if (nums[mid] > target) {
+              //search left
+              end = mid - 1;
+          } else {
+              //search right
+              start = mid + 1;
+          }
+      }
+      return -1;
+  };
+  const findLast = (nums, target) => {
+      let start = 0, end = nums.length - 1;
+      while (start <= end) {
+          let mid = Math.floor((start + end) / 2);
+          if (nums[mid] === target) {
+              if (mid === nums.length - 1 || nums[mid + 1] !== target) {
+                  return mid;
+              } else {
+                  //search right
+                  start = mid + 1;
+              }
+          } else if (nums[mid] > target) {
+              //search left
+              end = mid - 1;
+          } else {
+              //search right
+              start = mid + 1;
+          }
+      }
+      return -1;
+  }
+  return [findFirst(nums, target), findLast(nums, target)];
+};
+
 var searchRange = function(nums, target) {
   var start = 0, end = nums.length - 1;
   var result = [];
