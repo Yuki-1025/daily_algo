1d3eba75daee93eaadecbc4e5bcdebf0c9a95e2b
Yuki Li <yunqili1025@gmail.com>
2021-10-04 23:13:18 -0400

    4 types tree traversal

---
+++ Blind75/1004_mock.js
@@ -0,0 +1,50 @@
+ // in-order traversal: 5, 2, 6, 1, 7, 3,8
+ // pre-order traversal: 1, 2, 5, 6, 3, 7, 8
+ // post-order traversal: 5, 6, 2, 7, 8, 3, 1
+ // level-order traversal: 1,2,3,5,6,7,8
+
+function inOrder (root) {
+  // base case
+ if (root == null) { return; }
+ // recursive case
+ inOrder(root.left);
+ console.log(root.val);
+ inOrder(root.right);
+}
+
+function preOrder (root) {
+  // base case
+ if (root == null) { return; }
+ // recursive case
+ console.log(root.val);
+ preOrder(root.left);
+ preOrder(root.right);
+}
+
+function postOrder (root) {
+  //base case
+ if (root == null) { return; }
+ // recursive
+ postOrder(root.left);
+ postOrder(root.right);
+ console.log(root.val);
+}
+
+function levelOrder (root) {
+  // edge case
+    if (root == null) { return; }
+
+   var queue = [];
+   queue.push(root);
+
+   while (queue.length > 0 ) {
+     var current = queue.shift();
+     console.log(current.val);
+     if (current.left) {
+       queue.push(current.left);
+     }
+     if (current.right) {
+       queue.push(current.right)
+     }
+   }
+  }
