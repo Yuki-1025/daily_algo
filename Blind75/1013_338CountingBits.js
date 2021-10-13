71f4755472b4004eb81eab078677e4a97e45bf2e
Yuki Li <yunqili1025@gmail.com>
2021-10-13 17:53:18 -0400

    counting bits by string method

---
+++ Blind75/1013_338CountingBits.js
@@ -0,0 +1,38 @@
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+// ans[i] is the number of 1's in the binary representation of i.
+
+// Input: n = 2
+// Output: [0,1,1]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+
+// Input: n = 5
+// Output: [0,1,1,2,1,2]
+// Explanation:
+// 0 --> 0
+// 1 --> 1
+// 2 --> 10
+// 3 --> 11
+// 4 --> 100
+// 5 --> 101
+
+// STRING METHOD: use toString(2): 二进制string
+const checkOne = (str) => {
+  var count = 0;
+  for (let i = 0; i < str.length; i ++) {
+      if (str[i] === '1') {
+          count ++;
+      }
+  }
+  return count;
+}
+
+var countBits = function(n) {
+  var binary = [];
+  for (let i = 0; i <= n; i++) {
+      binary.push(checkOne(i.toString(2)));
+  }
+  return binary;
+};
