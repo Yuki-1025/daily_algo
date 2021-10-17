1e34d43d962677fa7c483d4e6147b716f7ef378f
Yuki Li <yunqili1025@gmail.com>
2021-10-17 16:05:29 -0400

    using map instead

--- Blind75/1017_oa_sortTheSummary.js
+++ Blind75/1017_oa_sortTheSummary.js
@@ -31,3 +31,37 @@ function sortSummary (arr) {
 
   return output;
 }
+
+// MAP VS OBJECT
+// keys of object are strings; keys of map can be any value
+// can get the size of a map: map.size; need track size of obj
+var map = new Map();
+map.set('key', 1);
+map.get('key');
+if(map.has('key'));
+map.delete('key');
+map.size;
+// ======using map instead of obj
+function sortSummary (arr) {
+  // store num and freq in a map
+  var frequency = new Map();
+  for (let i = 0; i < arr.length; i ++) {
+    if (!frequency.has(arr[i])) {
+      frequency.set(arr[i], 1);
+    } else {
+      frequency.set(arr[i], frequency.get(arr[i]) + 1);
+    }
+  }
+  // convert the map into array; i could use Object.entries(), but then the keys would be string['3', 2]
+  var output = Array.from(frequency.entries());// OR [...frequency.entries()]
+
+  // sort new array
+  output.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0]
+    }
+    return b[1] - a[1];
+  })
+
+  return output;
+}
