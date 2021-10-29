d2d6a73f943d4bb28385526cdcc258619989fd10
Yuki Li <yunqili1025@gmail.com>
2021-10-28 20:55:16 -0400

    improve space complexity

--- Blind75/1004_79WordSearch.js
+++ Blind75/1004_79WordSearch.js
@@ -62,29 +62,54 @@ var exist = function(board, word) {
   return result;
 };
 
-// var exist = function(board, word) {
-//   // create a matrix for recording [visited or not]
-//   var visit = [];
-//   for (let k = 0; k < board.length; k ++) {
-//       visit.push(new Array(board[k].length).fill(false));
-//   }
+// IMPROVE THE SPACE COMPLEXITY BY not creating a record matrix
+var exist = function(board, word) {
+   // store visited record in the original cell
+    var result = false;
+    var findOne = (i, row, col) => {
+        if (i === word.length) {
+            result = true;
+            return;
+        }
+        if (i === 0) {
+            for (let row = 0; row < board.length; row ++) {
+                for (let col = 0; col < board[0].length; col ++) {
+                    if (board[row][col] === word[i]) {
+                        let temp = board[row][col];
+                        board[row][col] = 0;
+                        i ++;
+                        findOne(i, row, col);
+                        i --;
+                        board[row][col] = temp;
+                    }
+                }
+            }
+        }
+        if (i > 0) {
+            let neighbors = findAdjacent(row, col, board);
+            for (let neighbor of neighbors) {
+                if (board[neighbor[0]][neighbor[1]] && board[neighbor[0]][neighbor[1]] === word[i]) {
+                    let temp = board[neighbor[0]][neighbor[1]];
+                    board[neighbor[0]][neighbor[1]] = 0;
+                    i ++;
+                    findOne(i, neighbor[0], neighbor[1]);
+                    i --;
+                    board[neighbor[0]][neighbor[1]] = temp
+                }
+            }
+        }
+    }
+    findOne(0);
+    return result;
+};
 
-//   var checkLetter = (letter, board) => {
-//       for (let row = 0; row < board.length; row ++) {
-//           for (let col = 0; col < board[row].length; col ++) {
-//               if (board[row][col] === letter && !visit[row][col]) {
-//                   visit[row][col] = true;
-//                   return true;
-//               }
-//               //return false;
-//           }
-//       }
-//       return false;
-//   }
-//   for (let n = 0; n < word.length; n ++) {
-//       if(!checkLetter(word[n], board)) {
-//           return false;
-//       }
-//   }
-//   return true;
-// };
+var findAdjacent = (row, col, board) => {
+    let maxR = board.length - 1;
+    let maxC = board[0].length - 1;
+    var neighbors = [];
+    if (row - 1 >= 0) { neighbors.push([row - 1, col]); }
+    if (col - 1 >= 0) { neighbors.push([row, col - 1]); }
+    if (row + 1 <= maxR) { neighbors.push([row + 1, col]); }
+    if (col + 1 <= maxC) { neighbors.push([row, col + 1]); }
+    return neighbors;
+}
