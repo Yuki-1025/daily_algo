66618d547384119357a6606cb1f55709317c108a
Yuki Li <yunqili1025@gmail.com>
2021-10-31 01:48:52 -0400

    w/ bt finally find the error reason

--- Blind75/0930_141LinkedListCycle.js
+++ Blind75/0930_141LinkedListCycle.js
@@ -1,6 +1,8 @@
 // Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
-// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+// There is a cycle in a linked list if there is some node in the list that can be reached again by
+// continuously following the next pointer. Internally, pos is used to denote the index of the node
+// that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
 // Return true if there is a cycle in the linked list. Otherwise, return false.
 
@@ -12,16 +14,24 @@
 var hasCycle = function(head) {
   //edge case
   if (head == null) {
-      return false;
+    return false;
   }
   var slow = head;
   var fast = head;
   while (fast.next != null && fast.next.next != null) {//只check fast即可
-      slow = slow.next;
-      fast = fast.next.next;
-      if (slow == fast) {
-          return true;
-      }
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow == fast) {
+    return true;
+    }
   }
   return false;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
---
+++ Blind75/1029_39CombinationSum.js
@@ -0,0 +1,60 @@
+// Given an array of distinct integers candidates and a target integer target, return a list of all
+//unique combinations of candidates where the chosen numbers sum to target. You may return the
+//combinations in any order.
+
+// The same number may be chosen from candidates an unlimited number of times. Two combinations are
+//unique if the frequency of at least one of the chosen numbers is different.
+
+// It is guaranteed that the number of unique combinations that sum up to target is less than 150
+//combinations for the given input.
+
+var combinationSum = function(candidates, target) {
+  var cbs = new Set();
+  var cb = [];
+  var sum = 0;
+  const calculation = () => {
+      // console.log(cb);
+      if (sum === target) {
+          var sorted_cb = cb.slice();
+          sorted_cb.sort();
+          cbs.add(sorted_cb.toString());
+          return;
+      }
+      if (sum > target) { return; }
+      for (let c = 0; c < candidates.length; c++) {
+          sum += candidates[c];
+          cb.push(candidates[c]);
+          calculation();
+          sum -= candidates[c];
+          cb.pop();
+      }
+  }
+
+  calculation();
+  var output = [];
+  cbs.forEach((s) => {
+      output.push(s.split(','));
+  })
+  return output;
+};
+
+//???????? why startIndex? to avoid duplicates
+var combinationSum = function(candidates, target) {
+  let result = [];
+
+  function dfs(current, startIndex, currentSum){
+      if(currentSum == target) {
+          result.push(current);
+          return;
+      }
+      if(currentSum > target) {
+          return;
+      }
+      for (let i = startIndex; i < candidates.length; i++) {
+          dfs(current.concat(candidates[i]), i, currentSum + candidates[i]);
+      }
+  }
+
+  dfs([],0, 0);
+  return result;
+};
