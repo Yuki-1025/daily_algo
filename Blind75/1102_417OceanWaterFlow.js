f4397afda255aabd6d6d4419354208c91e86f9fe
Yuki Li <yunqili1025@gmail.com>
2021-11-04 11:54:23 -0400

    DFS speed up

--- Blind75/1102_417OceanWaterFlow.js
+++ Blind75/1102_417OceanWaterFlow.js
@@ -15,6 +15,50 @@
 Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
 Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
 
+// USE DFS is more suitable for this problem: starting from the boundary cells
+var pacificAtlantic = function(heights) {
+    var output = [];
+    var lenR = heights.length, lenC = heights[0].length;
+    var pacific = Array(lenR).fill(0).map((row) => Array(lenC).fill(0));
+    var atlantic = Array(lenR).fill(0).map((row) => Array(lenC).fill(0));
+    // only loop through the boundaries
+    for (let row = 0; row < lenR; row ++) {
+        dfs(row, 0, pacific, heights);
+        dfs(row, lenC-1, atlantic, heights);
+    }
+    for (let col = 0; col < lenC; col ++) {
+        dfs(0, col, pacific, heights);
+        dfs(lenR-1, col, atlantic, heights);
+    }
+    for (let row = 0; row < lenR; row ++) {
+        for (let col = 0; col < lenC; col ++) {
+            if (pacific[row][col] && atlantic[row][col]) output.push([row, col]);
+        }
+    }
+    return output;
+};
+
+var dfs = (i, j, visited, board) => {
+    visited[i][j] = 1;
+    let children = flowTo([i, j], board);
+    for (let c of children) {
+        if (!visited[c[0]][c[1]]) {
+            dfs(c[0], c[1], visited, board);
+        }
+    }
+}
+
+var flowTo = ([i, j], board) => {
+    let output = [];
+    let lenV = board.length, lenH = board[0].length;
+    let curr = board[i][j];
+    if (i-1 >= 0 && board[i-1][j] >= curr) output.push([i-1, j]);
+    if (i+1 < lenV && board[i+1][j] >= curr) output.push([i+1, j]);
+    if (j-1 >= 0 && board[i][j-1] >= curr) output.push([i, j-1]);
+    if (j+1 < lenH && board[i][j+1] >= curr) output.push([i, j+1]);
+    return output;
+}
+
 // BASIC solution: from every cell, grow a graph then BFS
 var pacificAtlantic = function(heights) {
   var output = [];
@@ -65,3 +109,5 @@ var checkRoute = (arr, len1, len2) => {
   }
   return pacific && atlantic;
 }
+
+
