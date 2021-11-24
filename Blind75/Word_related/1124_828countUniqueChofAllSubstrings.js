5beb0885414cd12599a417f50ab590899eab73ba
Yuki Li <yunqili1025@gmail.com>
2021-11-24 16:42:02 -0500

    like sum of subarray min

--- Blind75/Word_related/1124_828countUniqueChofAllSubstrings.js
+++ Blind75/Word_related/1124_828countUniqueChofAllSubstrings.js
@@ -11,6 +11,44 @@
 // Evey substring is composed with only unique letters.
 // Sum of lengths of all substring is 1 + 1 + 1 + 2 + 2 + 3 = 10
 
+// SIMILAR TO FIND THE MIN OF EVERY SUBARRAY ================================================
+// Instead of counting all unique characters and struggling with all possible substrings,
+// we can count for every char in S, how many ways to be found as a unique char.
+// We count and sum, and it will be out answer.
+Think about string "XAXAXXAX" and focus on making the second "A" a unique character.
+We can take "XA(XAXX)AX" and between "()" is our substring.
+We can see here, to make the second "A" counted as a uniq character, we need to:
+
+insert "(" somewhere between the first and second A
+insert ")" somewhere between the second and third A
+For step 1 we have "A(XA" and "AX(A", 2 possibility.
+For step 2 we have "A)XXA", "AX)XA" and "AXX)A", 3 possibilities.
+
+So there are in total 2 * 3 = 6 ways to make the second A a unique character in a substring.
+In other words, there are only 6 substring, in which this A contribute 1 point as unique string.
+
+var uniqueLetterString = function(s) {
+  var res = 0;
+  var pos = {};
+  // loop the s once to store locations
+  for (let i = 0; i < s.length; i++) {
+      if (!pos[s[i]]) pos[s[i]] = [];
+      pos[s[i]].push(i);
+  }
+  let len = s.length;
+  //let mod = 1e9 +7;
+  for (let key in pos) {
+      let last = -1;
+      for (let i = 0; i < pos[key].length; i++) {
+          let next = pos[key][i+1] === undefined? len:pos[key][i+1];
+          res = (res + (pos[key][i] - last) * (next - pos[key][i]));
+          last = pos[key][i];
+      }
+  }
+
+  return res;
+};
+
 // ============= TIME EXCEED ====================================================
 // brutal force - first find out all substrings, then calculate unique num for each
 var uniqueLetterString = function(s) {
